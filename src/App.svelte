8462f2c316229cf2aa26fe7dcab0f838b4e376b8
Hui Peng Hu <woohp135@gmail.com>
2022-01-06 01:34:54 -0800

    download the .torrent file from yts instead of using magnet

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { Buffer } from 'buffer';
     import { onMount } from 'svelte';
     import * as WebTorrent from 'webtorrent';
 
@@ -61,8 +62,9 @@
         if (movieInfoHash) {
             /* movieTitle = urlParams.get("n") || ''; */
 
-            const magnetURI = `magnet:?xt=urn:btih:${movieInfoHash}`;
-            /* const magnetURI = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'; */
+            const magnetURI = `https://yts.mx/torrent/download/${movieInfoHash}`;
+            const torrentResponse = await fetch(magnetURI);
+            const torrentContent = await torrentResponse.arrayBuffer();
             const opts = {
                 announce: [
                     'wss://tracker.btorrent.xyz',
@@ -71,8 +73,7 @@
                 ],
             };
 
-            console.debug(magnetURI);
-            client.add(magnetURI, opts, (torrent: WebTorrent.Torrent) => {
+            client.add(Buffer.from(torrentContent), opts, (torrent: WebTorrent.Torrent) => {
                 // Got torrent metadata!
                 console.info('Client is downloading:', torrent.infoHash);
 
